06e523375f84fdf31ef98edbdd3c7f8e6c9405a4
Radheshyam gupta <radheshyamjio52@gmail.com>
2024-03-04 11:51:05 +0530

    ...

--- Backend/index.js
+++ Backend/index.js
@@ -37,7 +37,7 @@ app.use('/backend/user',UserRouter)
 app.use(express.static(path.join(__dirname,'client/dist')))
 
 app.get('*',(req,res)=>{
-  res.sendFile(path.json(__dirname,'client','dist','index.html'))
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
 })
 
 app.use((err,req,res,next)=>{
